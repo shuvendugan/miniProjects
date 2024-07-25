8deed19502aba1a16326ac4b62b51a29ae4a0f3b
shuvendugan <shuvendugan@gmail.com>
2024-07-25 16:36:49 +0530

    some changes to background

--- background/index.php
+++ background/index.php
@@ -15,14 +15,21 @@
                     <span id="minimise" class="round"></span>
                     <span id="fullscreen" class="round"></span>
                 </div>
-                <div id="subject">GIT</div>
+                <!-- <div id="subject">GIT</div> -->
                 <!-- <div id="date">July 6, 2024</div> -->
                 <div id="date"><?php echo date("F j, Y"); ?></div>
             </div>
             <div id="text">
-                <p>$ git init </p>
-                <p>$ git status</p>
-                <p>$ git add . </p>
+                <p id='title'>GIT</p>
+                <!-- <p>$ git rm --cached [filename] //delete traking file from git cached to untracked</p>
+                <p>$ git rm --cached -r [foldername] //for folder</p>
+                <p>$ git resote --staged</p>
+                <p>$ git commit -am "message" // commit directly </p>
+                <p>$ git checkout [commitid] [filenaem] // resote deleted file</p>
+                <p>$ git config --global -e</p>
+                <p>$ git switch [branchname]</p>
+                <p>$ git switch -C [brachName] // create and switch between branch</p>
+                <p>$ git revert [commitid] -m 1 // merge!unmerged or reversed</p> -->
             </div>
             <div id="footer">
                 shuvendugan
