91ed408ed03ad8b2d699b000004f19f1a22cea99
shuvendugan <shuvendugan@sg.local>
2024-08-07 23:09:19 +0530

    Access modifiers

--- phpoop/accessModifiers.php
+++ phpoop/accessModifiers.php
@@ -2,5 +2,35 @@
 //Access modifiers decides wherewe can access the methods and properties
 //Access Zone
 //===================================
+//> Access inside the current class only - Private
+//> Access every where - public
+//> Access ininherited class -protected
+
+class Teachers{
+    private function questionPapers(){
+        echo "Question paper for student";
+    }
+    function exam(){
+        $this->questionPapers();
+    }
+
+    protected function studentMarks(){
+        echo "all student mark";
+    }
+}
+
+class Management extends Teachers{
+    function reviewExam(){
+        $this->studentMarks();
+    }
+}
+$t1 = new Teachers();
+// $t1->questionPapers(); //it give error as private cannot access from outside
+
+$t1->exam();
+// $t1->studentMarks(); // it give error as protected 
+
+$m1 = new Management();
+$m1->reviewExam();
 
 ?>
