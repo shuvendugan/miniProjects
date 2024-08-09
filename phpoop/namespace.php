e05882f1c456efadfda1ead386304791dcc06221
shuvendugan <shuvendugan@sg.local>
2024-08-09 16:06:29 +0530

    namespace learned

---
+++ phpoop/namespace.php
@@ -0,0 +1,8 @@
+<?php
+// this allow for better organization by grouping classes that work together to perform a task
+// they allow same name to be used for more than one class  
+
+// to better understand this i have created a namespace floder .. inside the phpoop floder
+
+
+?>
